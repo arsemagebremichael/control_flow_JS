5748b82c6504874a61a2b4956f3b4d4701292d9f
A <Arsema.arsemaagebremichael@gmail.com>
2025-04-23 08:23:42 +0300

    editing  answers to control flow questions

--- index.js
+++ index.js
@@ -1,31 +1,28 @@
-
-
 //Login attempts and Locking it after the 3rd try
-function loginAttempts(attempts){
- while(attempts<=3 && attempts>0){
-    console.log('You can try again.');
-    attempts +=1;
- }
- while (attempts>3){
-    console.log('Account locked.');
-    break;
- }
+function loginAttempts(attempts) {
+   while (attempts <= 3 && attempts > 0) {
+      console.log('You can try again.');
+      attempts += 1;
+   }
+   while (attempts > 3) {
+      console.log('Account locked.');
+      break;
+   }
 }
-loginAttempts(5)
+loginAttempts(1)
 console.log(''); //To give me a space between my logs
-
 
 
 //Logging the votes in our voting pool
 const votingPool = (votes) => {
    let vote = 0;
    let voteLength = votes.length;
-   do{
-      
+   do {
+
       console.log(`Recorded vote: ${votes[vote]}`)
-      vote +=1;
-      
-    }while(vote < voteLength)
+      vote += 1;
+
+   } while (vote < voteLength)
 
 }
 votingPool(['Useful', 'Not useful', 'Not practical'])
@@ -36,7 +33,7 @@ console.log(''); //To give me a space between my logs
 //Corresponding Days of the week with numbers
 const daysOfTheWeek = (daysInNumber) => {
    daysInNumber.map(day => {
-      switch(day){
+      switch (day) {
          case 1:
             console.log(`${day} corresponds Monday.`);
             break;
@@ -48,10 +45,10 @@ const daysOfTheWeek = (daysInNumber) => {
             break;
          case 4:
             console.log(`${day} corresponds Thursday.`);
-            break;  
+            break;
          case 5:
             console.log(`${day} corresponds Friday.`);
-            break; 
+            break;
          case 6:
             console.log(`${day} corresponds Saturday.`);
             break;
@@ -61,7 +58,7 @@ const daysOfTheWeek = (daysInNumber) => {
          default:
             console.log(`${day} done not correspond any day of the week.`);
             break;
-            }
+      }
    })
 }
 daysOfTheWeek([1, 2, 3, 4, 5, 6, 7])
@@ -70,13 +67,13 @@ console.log(''); //To give me a space between my logs
 
 
 //Password Strength check
-function passwordStrengthCheck(passwords){
+function passwordStrengthCheck(passwords) {
    let i;
-   passwords.map(password=>{
+   passwords.map(password => {
       if (password.length >= 8) {
          console.log(`The password ${password} is a strong password.`);
       }
-      else{
+      else {
          console.log(`The password ${password} ia not a strong password.`);
       }
    })
@@ -88,8 +85,8 @@ console.log(''); //To give me a space between my logs
 
 
 //Language Change
-const multilingualGreeting = (languageCode)=>{
-   switch(languageCode){
+const multilingualGreeting = (languageCode) => {
+   switch (languageCode) {
       case 'en':
          console.log('Hello');
          break;
@@ -115,17 +112,19 @@ console.log(''); //To give me a space between my logs
 
 
 //Weather dashboard
-function weatherAlert(weatherDashboard){
-   weatherDashboard.map(temprature=>{
-      if (temprature > 30){
+function weatherAlert(weatherDashboard) {
+   weatherDashboard.map(temprature => {
+      if (temprature > 30) {
          console.log('High heat alert!');
       }
-      else if (temprature < 15){
-         console.log('Cold alert!');}
+      else if (temprature < 15) {
+         console.log('Cold alert!');
+      }
       else {
          console.log('Normal conditions');
-      }}
-      )
+      }
+   }
+   )
 }
 weatherAlert([32, 40, 23, 12, 18, 9])
 console.log(''); //To give me a space between my logs
@@ -133,13 +132,13 @@ console.log(''); //To give me a space between my logs
 
 
 //Registering in queue
-function registarationQueue(queueOfNames){
+function registarationQueue(queueOfNames) {
    let lengthOfQueue = queueOfNames.length;
-   while(lengthOfQueue>0){
+   while (lengthOfQueue > 0) {
       console.log(`Name: ${queueOfNames.shift()}`);
-      lengthOfQueue-=1;
-   }  
-   console.log({queueOfNames});
+      lengthOfQueue -= 1;
+   }
+   console.log({ queueOfNames });
 }
 registarationQueue(['Arsema A. Gebremichael', 'Semhal Estifanos', 'Pheobe Gloria', 'Queen Carine']);
 console.log('');
@@ -147,16 +146,26 @@ console.log('');
 
 
 //Retake test until score is 50 and increase 10 on each attempt
-function scoreImprovement(score){
-   let  attempt = 1;
-   do{
-         console.log(`Your score is ${score} on attempt ${attempt}.`);
-         score = score+10;
-         attempt += 1;
-      }while(score<50);
+function scoreImprovement(score) {
+   let retakeAttempt = 1;
 
+   if (score >= 50) {
+      console.log(`With a score of ${score}, you don't need to retake the test.`);
+   }
+
+   else {
+      console.log(`Your score is ${score} before any attempts.`);
       
+      do {
+         score = score + 10;
+         console.log(`Your score is ${score} on attempt ${retakeAttempt}.`);
+         retakeAttempt += 1;
+      } while (score < 50);
+
+   }
+
+
 }
 
 
-scoreImprovement(0);
+scoreImprovement(50);
